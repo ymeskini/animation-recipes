9e787b051f3d3eeb8c54131fe861cd82e4e766dd
Youssef Meskini <youss.meskini@gmail.com>
2025-09-26 19:08:07 +0100

    refactor: enhance header component with bounded scroll functionality and blur effect

--- src/animations/header.tsx
+++ src/animations/header.tsx
@@ -3,41 +3,75 @@ import {
   useMotionValue,
   useMotionValueEvent,
   useScroll,
+  clamp,
+  useTransform,
+  useMotionTemplate,
 } from "motion/react";
 
-export function Header() {
+function useBoundedScroll(bound: number) {
   const { scrollY } = useScroll();
-  const height = useMotionValue(80);
+  const scrollYBounded = useMotionValue(0);
+  const scrollYBoundedProgress = useTransform(
+    scrollYBounded,
+    [0, bound],
+    [0, 1]
+  );
 
   useMotionValueEvent(scrollY, "change", (current) => {
     const previous = scrollY.getPrevious();
     const diff = current - (previous || 0);
-    const newHeight = height.get() - diff;
+    const newScrollYBounded = scrollYBounded.get() + diff;
 
-    height.set(Math.min(Math.max(newHeight, 50), 80));
+    scrollYBounded.set(clamp(0, bound, newScrollYBounded));
   });
+
+  return { scrollYBounded, scrollYBoundedProgress };
+}
+
+export function Header() {
+  const { scrollYBoundedProgress } = useBoundedScroll(100);
+  // const scrollYBoundedProgressThrottled = useTransform(
+  //   scrollYBoundedProgress,
+  //   [0, 0.75, 1],
+  //   [0, 0, 1]
+  // );
+  const height = useTransform(scrollYBoundedProgress, [0, 1], [80, 50]);
+  const opacity = useTransform(scrollYBoundedProgress, [0, 1], [1, 0]);
+  const logoScale = useTransform(scrollYBoundedProgress, [0, 1], [1, 0.9]);
+  const blurValue = useTransform(scrollYBoundedProgress, [0, 1], [1, 0.1]);
+  const concatBlurClass = useMotionTemplate`rgba(255, 255, 255, ${blurValue})`;
+
 
   return (
     <div className="mx-auto flex w-full max-w-3xl flex-1 overflow-hidden text-slate-600">
       <div className="z-0 flex-1 overflow-y-scroll">
         <motion.header
-          style={{ height }}
-          className="fixed inset-x-0 flex h-20 bg-white shadow"
+          style={{
+            height,
+            backgroundColor: concatBlurClass,
+          }}
+          className="fixed inset-x-0 flex h-20 backdrop-blur-md shadow"
         >
           <div className="mx-auto flex w-full max-w-3xl items-center justify-between px-8">
-            <p className="flex origin-left items-center text-xl font-semibold uppercase">
+            <motion.p
+              style={{ scale: logoScale }}
+              className="flex origin-left items-center text-xl font-semibold uppercase"
+            >
               <span className="-ml-1.5 inline-block -rotate-90 text-[10px] leading-[0]">
                 The
               </span>
               <span className="-ml-1 text-2xl tracking-[-.075em]">
                 Daily Bugle
               </span>
-            </p>
-            <nav className="flex space-x-4 text-xs font-medium text-slate-400">
+            </motion.p>
+            <motion.nav
+              style={{ opacity }}
+              className="flex space-x-4 text-xs font-medium text-slate-400"
+            >
               <a href="#">News</a>
               <a href="#">Sports</a>
               <a href="#">Culture</a>
-            </nav>
+            </motion.nav>
           </div>
         </motion.header>
 
